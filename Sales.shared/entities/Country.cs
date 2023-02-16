92d3093be2fa7c53408b0c1d847b84db2bdd9541
ESTIVEN ALONSO SEGURO ACOSTA <estivenseguro229799@correo.itm.edu.co>
2023-02-16 00:57:32 -0500

    Add data context

---
+++ Sales.shared/entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.shared.entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "país")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "el campo {0} no puede tener mas de {1} caractéres.")]
+        public string Name { get; set; } = null!;
+    }
+}
