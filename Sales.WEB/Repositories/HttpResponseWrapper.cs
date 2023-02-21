158504dc5dc19cbc9ee1376c8a613cdb01819c70
ESTIVEN ALONSO SEGURO ACOSTA <estivenseguro229799@correo.itm.edu.co>
2023-02-20 21:58:52 -0500

    Begin countries API controller

---
+++ Sales.WEB/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+
+namespace Sales.WEB.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Error = error;
+            Response = response;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public bool Error { get; set; }
+
+        public T? Response { get; set; }
+
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public async Task<string?> GetErrorMessage()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var StatusCode = HttpResponseMessage.StatusCode;
+            if (StatusCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrado";
+            }
+            else if (StatusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            else if (StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que logearte para hacer esta operación";
+            }
+            else if (StatusCode == HttpStatusCode.Forbidden)
+            {
+                return "No tienes permisos para hacer esta operación";
+            }
+
+            return "Ha ocurrido un error inesperado";
+        }
+    }
+}
