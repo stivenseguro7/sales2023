c24ecf9dda09312064c68f6bf09cca4229f62ecf
ESTIVEN ALONSO SEGURO ACOSTA <estivenseguro229799@correo.itm.edu.co>
2023-03-02 14:27:44 -0500

    Crud Categories

--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -11,4 +11,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7195/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+
 await builder.Build().RunAsync();
