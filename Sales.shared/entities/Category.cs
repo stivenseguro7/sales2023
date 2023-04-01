6522a7f946817d12b2120dcf3bd5718b300184b0
ESTIVEN ALONSO SEGURO ACOSTA <estivenseguro229799@correo.itm.edu.co>
2023-04-01 01:36:15 -0500

    complete categories CRUD

--- Sales.shared/entities/Category.cs
+++ Sales.shared/entities/Category.cs
@@ -1,4 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Sales.shared.entities
 {
@@ -6,9 +12,9 @@ namespace Sales.shared.entities
     {
         public int Id { get; set; }
 
-        [Display(Name = "Categoria")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caractéres")]
+        [Display(Name = "Categoría")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; } = null!;
     }
 }
