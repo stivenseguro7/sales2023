366b96db2a1c5f5771cefdbc3ed3cdd3aa60d507
ESTIVEN ALONSO SEGURO ACOSTA <estivenseguro229799@correo.itm.edu.co>
2023-04-11 19:05:58 -0500

    AddProductsTables

--- Sales.shared/entities/Category.cs
+++ Sales.shared/entities/Category.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Sales.shared.entities
 {
@@ -16,5 +10,11 @@ namespace Sales.shared.entities
         [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; } = null!;
+
+        public ICollection<ProductCategory>? ProductCategories { get; set; }
+
+        [Display(Name = "Productos")]
+        public int ProductCategoriesNumber => ProductCategories == null ? 0 : ProductCategories.Count;
+
     }
 }
